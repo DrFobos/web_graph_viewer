6fc41cf1551b8e7f5a1c7786e045c35766b40906
Aleksey Kondratov <frodoklg@mail.ru>
2018-02-13 09:14:08 +0300

    comment block of code in app.py which was using during working on project

--- app.py
+++ app.py
@@ -58,24 +58,24 @@ def change_graph():
     })
 
 # Uncomment to prevent browser caching and see changes in static files
-
-import os
-from flask import url_for
-
-
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+#
+# import os
+# from flask import url_for
+#
+#
+# @app.context_processor
+# def override_url_for():
+#     return dict(url_for=dated_url_for)
+#
+#
+# def dated_url_for(endpoint, **values):
+#     if endpoint == 'static':
+#         filename = values.get('filename', None)
+#         if filename:
+#             file_path = os.path.join(app.root_path,
+#                                      endpoint, filename)
+#             values['q'] = int(os.stat(file_path).st_mtime)
+#     return url_for(endpoint, **values)
 
 
 if __name__ == "__main__":
